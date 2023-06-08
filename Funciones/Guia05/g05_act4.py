dbfbee884618168befe88c14654f6e7b5ca73a52
Agustin Ceballos <101526848+AgustinCeballos37@users.noreply.github.com>
2023-06-08 00:05:53 -0300

    Delete g05_act4.py

--- Funciones/Guia05/g05_act4.py
+++
@@ -1,7 +0,0 @@
-def cantidadDePalabra(cadena):
-    cadenaSplit = cadena.split()
-    contador = 0
-    for palabra in cadenaSplit:
-        contador += 1
-    return contador
-print(cantidadDePalabra("Quiero comer manzanas, solamente manzanas."))
